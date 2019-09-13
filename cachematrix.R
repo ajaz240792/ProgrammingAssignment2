51351ba6c2bda8088b918747e88e5829b5dfa2ac
ajaz240792 <ajaz240792@gmail.com>
2019-09-14 00:57:59 +0200

    homework first edition

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This function creates a special 'matrix' object that can cache its inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { # Define the argument with default mode of 'matrix'
+        Matx_inverse <- NULL # We initialize 'Matx_inverse' as NULL. It will hold value of matrix inverse
+        
+        set <- function(y) {
+                x <<- y
+                Matx_inverse <<- NULL
+        } # We defined the 'set' function to assign the new value of the matrix
+        # in parent environment
+        
+        get <- function() {
+                x
+        } # Return value of the matrix argument
+        
+        setinverse <- function(inverse) {
+                Matx_inverse <<- inverse
+        } # Assign value of 'Matx_inverse' in the parent environment
+        
+        getinverse <- function() {
+                Matx_inverse
+        } # Get the value of 'Matx_inverse' where called
+        
+        list(set = set, get = get, setinverse = setinverse,
+             getinverse = getinverse)
 }
 
+# This function computes the inverse of the special 'matrix' returned by 'makeCacheMatrix'
+# function
 
-## Write a short comment describing this function
+# If the inverse has been already calculated (and the matrix has not changed),
+# the   'cacheSolve' function (bellow) will retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        Matx_inverse <- x$getinverse()
+        
+        if(!is.null(Matx_inverse)) {
+                message('getting cached data')
+                return(Matx_inverse)
+        }
+        
+        data <- x$get() # Get the original matrix data
+        Matx_inverse <- solve(data, ...) # Use 'solve()' function to inverse the matrix
+        x$setinverse(Matx_inverse) # Set the invertible matrix
+        Matx_inverse # Return the invertible matrix
 }
